539a47c8901c2ebbf7ee1d3c4aa304c5c6744c9c
Joseph Sanchez <jmichaelsanchez@gmail.com>
2022-04-16 17:56:08 -0500

    Complete

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,10 @@
 import { HashRouter as Router, Route } from 'react-router-dom';
-import './App.css';
 import StepOne from './StepOne/StepOne';
 import StepTwo from './StepTwo/StepTwo';
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <h1>Solo Project Spike</h1>
       <h2>Password Generator</h2>
       <Router>
